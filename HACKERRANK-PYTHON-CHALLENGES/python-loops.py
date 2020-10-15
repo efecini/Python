16c6952b3d100e9dee43b891bea602df3ddfac0d
Efe Cini <efecini@gmail.com>
2020-10-16 01:42:06 +0300

    python-loops added

---
+++ HACKERRANK-PYTHON-CHALLENGES/python-loops.py
@@ -0,0 +1,44 @@
+"""
+The provided code stub reads and integer, , from STDIN. For all non-negative integers , print .
+
+Example
+
+The list of non-negative integers that are less than  is . Print the square of each number on a separate line.
+
+0
+1
+4
+Input Format
+
+The first and only line contains the integer, .
+
+Constraints
+
+
+Output Format
+
+Print  lines, one corresponding to each .
+
+Sample Input 0
+
+5
+Sample Output 0
+
+0
+1
+4
+9
+16
+"""
+
+if __name__ == '__main__':
+    n = int(input())
+    
+    #[expression for item in list]  - Flat is better than nested” — The Zen of Python
+    [print(f"{item**2}") for item in range(0, n)]
+    
+    """
+    Not a good idea to use 'for' if you don't have to.
+    for item in range(0, n):
+        print(item**2)
+    """
